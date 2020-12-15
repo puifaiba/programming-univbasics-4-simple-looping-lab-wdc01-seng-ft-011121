2ccbf1d4c7822a130dc2292fe3afdc446d8e82d6
Boonie Attawut <puifai.ba@gmail.com>
2020-12-15 21:48:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times(string)
     counter += 1
   end
 end
+
+def loop_message_n_times(string, n)
+  counter = 0
+  while counter < n do
+    puts string
+    counter += 1
+  end
+end
